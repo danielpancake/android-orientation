fc58644fc77f4c95b9bc235e825473194b2469a3
danielpancake <dsatarov@outlook.com>
2021-02-24 12:34:48 +0500

    More orientation modes

--- dependencies/android/src/main/java/org/haxe/extension/AndroidOrientation.java
+++ dependencies/android/src/main/java/org/haxe/extension/AndroidOrientation.java
@@ -4,16 +4,26 @@ import android.content.pm.ActivityInfo;
 import org.haxe.extension.Extension;
 
 public class AndroidOrientation extends Extension {
-    public static final int ORIENTATION_PORTRAIT = 1;
-    public static final int ORIENTATION_LANDSCAPE = 2;
-
     private static int fixedOrientation = 0;
+
+    public static final int PORTRAIT = 1;
+    public static final int REVERSE_PORTRAIT = 2;
+    public static final int SENSOR_PORTRAIT = 3;
+    public static final int LANDSCAPE = 4;
+    public static final int REVERSE_LANDSCAPE = 5;
+    public static final int SENSOR_LANDSCAPE = 6;
 
     public static void setRequestedOrientation(int screenOrientation) {
         int requestedOrientation;
         switch(screenOrientation) {
-            case ORIENTATION_PORTRAIT: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT; break;
-            case ORIENTATION_LANDSCAPE: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE; break;
+            case PORTRAIT: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT; break;
+            case REVERSE_PORTRAIT: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_PORTRAIT; break;
+            case SENSOR_PORTRAIT: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT; break;
+
+            case LANDSCAPE: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE; break;
+            case REVERSE_LANDSCAPE: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_REVERSE_LANDSCAPE; break;
+            case SENSOR_LANDSCAPE: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE; break;
+            
             default: requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED;
         }
         
---
+++ readme.md
@@ -0,0 +1,30 @@
+# android-orientation
+Haxe extension which allows you to set screen orientation on android
+
+## Installation
+```bash
+haxelib git android-orientation https://github.com/danielpancake/android-orientation.git
+```
+
+Precompiled libraries are included, but it is recommended you compile them yourself:
+```bash
+lime rebuild android-orientation android
+```
+
+## Usage
+- Project.xml:
+```xml
+<!-- Add this line in the libraries section of your Project.xml file -->
+<haxelib name="AndroidOrientation" />
+```
+
+- In any haxe package of your project:
+```haxe
+import extension.androidorientation.AndroidOrientation;
+
+...
+
+AndroidOrientation.setScreenOrientation(AndroidOrientation.SENSOR_LANDSCAPE);
+// or UNSPECIFIED, PORTRAIT, REVERSE_PORTRAIT, SENSOR_PORTRAIT, LANDSCAPE, REVERSE_LANDSCAPE
+```
+It is advised to call this function as early as possible (preferably in preloader) to avoid screen cropping.
